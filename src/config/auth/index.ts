ee3b8bdbb4c0c477e5fabca6bd9b6668be12065f
Frost7994 <tommybyrne9@gmail.com>
2024-10-23 20:29:49 +0100

    feat(db): connected db to auth, persisting the user

--- src/config/auth/index.ts
+++ src/config/auth/index.ts
@@ -1,8 +1,24 @@
+import { db } from "@/config/drizzle";
+import {
+  accounts,
+  authenticators,
+  sessions,
+  users,
+  verificationTokens,
+} from "@/config/drizzle/schema";
 import env from "@/data/env";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { AuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: AuthOptions = {
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+    authenticatorsTable: authenticators,
+  }),
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
